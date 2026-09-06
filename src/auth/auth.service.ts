eede5e0155204dfec2437813bed1e7cf2868dc3e
Didac <didacesteban@gmail.com>
2026-09-06 10:57:31 +0200

    Adds auth endpoints. Add first version of create user

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    
+    try {
+      const user = this.userRepository.create(createUserDto);
+      await this.userRepository.save(user);
+      return user;
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
+
+  }
+
+  // ponemos never ya que nunca va a retornar nada porque estamos lanzando una excepción
+  private handleDBErrors(error: any): never {
+    if (error.code === '23505') {
+      throw new BadRequestException('User already exists');
+    }
+    throw new InternalServerErrorException('Please check server logs');
+  }
+}
