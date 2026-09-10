105b118903eed15e041c46483ae32185f072b872
Didac <didacesteban@gmail.com>
2026-09-10 13:24:29 +0200

    Add Auth decorator to product endpoints

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,12 +3,16 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/interfaces/valid-roles';
 
 @Controller('products')
+// @Auth() si lo ponemos aqui hariamos que para consultar estos endpoints hay que estar authenticado
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) { }
 
   @Post()
+  @Auth(ValidRoles.admin)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
@@ -24,11 +28,13 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin)
   update(@Param('id', ParseUUIDPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
   }
